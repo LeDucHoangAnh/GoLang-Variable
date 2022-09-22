cae3cb7d1dae07e4c15f9d7ec4f80bc55d61cc36
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-22 20:31:32 +0700

    Cách khai báo Type Inference

--- main.go
+++ main.go
@@ -10,4 +10,8 @@ func main() {
 
 	var number1 int = 11
 	fmt.Println(number1)
+	//Type Inference: khi khai báo go sẽ tự hiểu biến bạn khai bao thuộc kiểu nào.
+
+	var email = "kai@gmail.com"
+	fmt.Println(email)
 }
